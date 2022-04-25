7469001673035a3f5663f75e0e6305fc61439bc8
Jan Šindler <jan.sindler@educhem.cz>
2022-04-25 09:06:18 +0200

    Přidejte soubory projektu.

---
+++ MetroApp/LinkaMetra.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroApp
+{
+    internal class LinkaMetra
+    {
+        private int _pocetStanic;
+
+        private List<string> _nazvyStanice; //názvy stanice - Florenc, Pankrác, Hlavní nádraží, ...
+
+        public LinkaMetra(List<string> _vkladanestanice)
+        {
+            _nazvyStanice = _vkladanestanice;
+        }
+        public void VypisStanice()
+        {
+            foreach (string stanice in _nazvyStanice)
+            {
+                Console.WriteLine(stanice);
+            }
+        }
+        public void VypisPrvni()
+        {
+            Console.WriteLine(_nazvyStanice[0]);
+        }
+        public int ZjistiCas(string prvniStanice, string druhaStanice)
+        {
+            int cas = 0;
+            if(prvniStanice == druhaStanice)
+            {
+                Console.WriteLine("Stanice se shodují");
+                return cas;
+            }
+            else
+            {
+                //seznam musí obsahovat obě zadané stanice
+                if(_nazvyStanice.Contains(prvniStanice) && _nazvyStanice.Contains(druhaStanice))
+                {
+                    int indexPrvni = _nazvyStanice.IndexOf(prvniStanice);
+                    int indexDruhe = _nazvyStanice.IndexOf(druhaStanice);
+                    int rozdíl = indexPrvni - indexDruhe;
+                    cas = rozdíl * 2;
+                }
+            }
+            return cas;
+
+        }
+    }
+}
---
+++ MetroApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pražské metro");
+            List<String> linkaA = new List<string>
+            {"Nemocnice Motol", "Petřiny", "Nádrží Veleslaví", "Bořislav", "Dejvická", "Hradčanská", "Malostranská", "Staroměstská", "Muzeum",
+            "Nměstí Míru", "Jiřího z Poděbrad", "Flora", "Želivského", "STRAŠNICKÁ", "skalka", "Dope Hostivař"
+            };
+
+            LinkaMetra linkaMetroA = new LinkaMetra(linkaA);
+            linkaMetroA.VypisPrvni();
+            Console.WriteLine("------------");
+
+            linkaMetroA.VypisStanice();
+            Console.WriteLine("------------");
+
+            int cas = linkaMetroA.ZjistiCas("Nemocnice Motol", "Bořislav");//vracim se včase!!!!
+            Console.WriteLine(cas);
+
+            Console.ReadKey();
+        }
+    }
+}
